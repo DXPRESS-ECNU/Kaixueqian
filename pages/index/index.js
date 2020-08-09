f5858728a2d839a8dfcececfa601276ede533a37
Qucheng Miao <qcmiao@hotmail.com>
2020-08-09 14:30:29 +0800

    alpha 2

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,10 +11,8 @@ Page({
     CONFIG: {},
     URL_PREFIX: null,
     choose: null,
-    outputConfig: {},
-    isauth: true,
-    exportbtnDis: false,
-    exportbtnText: "分享图片"
+    shareJson: null,
+    exportbtnMode: 1
   },
   //事件处理函数
   bindViewTap: function() {
@@ -79,103 +77,72 @@ Page({
     var id = event.currentTarget.dataset.index
     var items = this.data.CONFIG[id].items
     var choose = items[Math.floor(Math.random() * items.length)]
+    this.shareJson = this.data.CONFIG[id].share
     this.setData({
-      choose,
-      outputConfig: {
-        avatarposi: this.data.CONFIG[id].avatarposi,
-        nicknameposi: this.data.CONFIG[id].nicknameposi
-      }
+      choose
     })
     console.log(this.data.choose)
     console.log(this.data.outputConfig)
   },
   bindReChoose: function() {
     this.setData({
+      exportbtnMode: 1,
+      outputConfig: null,
       choose: null
     })
   },
   openSetting() {
     wx.openSetting();
     this.setData({
-      exportbtnDis: false,
-      exportbtnText: "分享图片",
+      exportbtnMode: 1,
       isauth: true
     });
   },
   bindExport: function() {
     console.info(this.data.userInfo)
-    wx.getImageInfo({
-      src: this.data.userInfo.avatarUrl,
-      success: res => {
-        app.globalData.INDEX.avatarTemp = res.path
-        wx.getImageInfo({
-          src: app.globalData.INDEX.data.URL_PREFIX + app.globalData.INDEX.data.choose,
-          success: res => {
-            app.globalData.INDEX.chooseTemp = res.path
-            var outputConfig = app.globalData.INDEX.data.outputConfig
-            outputConfig.backgroundsize = { width: res.width, height: res.height }
-            app.globalData.INDEX.setData({
-              outputConfig
-            })
-            app.globalData.INDEX.createOutputImage()
+    this.shareJson = JSON.parse(JSON.stringify(this.shareJson).replace("{{background}}", this.data.URL_PREFIX + this.data.choose).replace("{{avatar}}", this.data.userInfo.avatarUrl).replace("{{nickname}}", this.data.userInfo.nickName))
+    this.setData({
+      shareJson: this.shareJson
+    })
+  },
+  bindSaveImage: function(event) {
+    console.log(event)
+
+    wx.saveImageToPhotosAlbum({
+      filePath: event.detail.path,
+      success() {
+        console.log("Saved")
+        app.globalData.INDEX.setData({
+          exportbtnMode: 2
+        })
+      },
+      fail(res) {
+        console.log(res.errMsg)
+        wx.getSetting({
+          success(res) {
+            if (!res.authSetting['scope.writePhotosAlbum']) {
+              app.globalData.INDEX.setData({
+                exportbtnMode: 3
+              })
+              wx.authorize({
+                scope: 'scope.writePhotosAlbum',
+                success() {
+                  app.globalData.INDEX.setData({
+                    exportbtnMode: 1
+                  })
+                },
+                fail() {
+                  console.error("Authorization Failed")
+                  app.globalData.INDEX.setData({
+                    exportbtnMode: 3
+                  })
+                }
+              })
+            }
           }
         })
       }
     })
-  },
-  createOutputImage: function() {
-    var context = wx.createCanvasContext('outputImg')
-    context.drawImage(this.chooseTemp, 0, 0);
-    context.drawImage(this.avatarTemp, this.data.outputConfig.avatarposi[0], this.data.outputConfig.avatarposi[1]);
-    context.setTextAlign("center")
-    context.fillText(this.data.userInfo.nickName, this.data.outputConfig.nicknameposi[0], this.data.outputConfig.nicknameposi[0])
-
-    context.draw(true, function () {
-      wx.canvasToTempFilePath({
-        canvasId: "outputImg",
-        quality: 1,
-        destWidth: app.globalData.INDEX.data.outputConfig.backgroundsize.width,
-        destHeight: app.globalData.INDEX.data.outputConfig.backgroundsize.height,
-        success(res) {
-          wx.saveImageToPhotosAlbum({
-            filePath: res.tempFilePath,
-            success() {
-              console.log("Saved")
-              app.globalData.INDEX.setData({
-                exportbtnDis: true,
-                exportbtnText: "已保存"
-              });
-            },
-            fail(res) {
-              console.log(res.errMsg)
-              wx.getSetting({
-                success(res) {
-                  if (!res.authSetting['scope.writePhotosAlbum']) {
-                    needauth()
-                    wx.authorize({
-                      scope: 'scope.writePhotosAlbum',
-                      success() {
-                        app.globalData.INDEX.setData({
-                          exportbtnDis: false,
-                          exportbtnText: "分享图片"
-                        });
-                      },
-                      fail() {
-                        console.error("Authorization Failed")
-                        app.globalData.INDEX.setData({
-                          isauth: false
-                        });
-                      }
-                    })
-                  }
-                }
-              })
-            }
-          })
-        }
-      })
-    })
-
 
   }
 })
